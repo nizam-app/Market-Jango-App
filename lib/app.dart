27283cdd5bd731cac7b3ab83d6238dc377231005
nizam-app <appswithnizam@gmail.com>
2025-07-16 04:27:17 +0600

    This is workin now

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:market_jango/routes/app_routes.dart';
-
-import 'bindings/controller_binding.dart';
-import 'core/utils/text_translations.dart';
-import 'core/utils/translation_text.dart';
 
 class App extends StatefulWidget {
   const App({super.key});
@@ -16,14 +11,8 @@ class App extends StatefulWidget {
 class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      initialBinding: BindingController(),
-      translations: AppTranslations(),
-      locale: Locale('en'),
-      fallbackLocale: Locale("bn"),
-      theme: themeChanger.themeMode,
-      onGenerateRoute: AppRoutes.routes,
-      debugShowCheckedModeBanner: false,
+    return MaterialApp.router(
+      routerConfig: router,
     );
   }
 }
